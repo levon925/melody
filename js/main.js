1861051a6caa25567e3bf51a30d0b2c404cf4844
levon925 <76520734+levon925@users.noreply.github.com>
2021-12-14 02:50:11 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    var currentFloor = 2;
+    var floorPath = $(".home-image path")
+    var counterUp = $('.counter-up')
+    var counterDown = $('.counter-down')
+    var modal = $(".modal")
+    var modalCloseButton = $(".modal-close-button")
+    var viewFlatsButton = $(".view-flats")
+
+    floorPath.on("mouseover",function(){
+        floorPath.removeClass("current-floor")
+        currentFloor = $(this).attr("data-floor");
+        $('.counter').text(currentFloor)
+    })
+    viewFlatsButton.on('click',function(){
+        modal.toggleClass('is-open')
+    })
+    floorPath.on('click', function(){
+        modal.toggleClass('is-open')
+    })
+    modalCloseButton.on('click', function(){
+        modal.toggleClass('is-open')
+    })
+
+    counterUp.on('click', function(){
+        if(currentFloor < 18){
+            currentFloor++
+            usCurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, useGrouping: false})
+            $('.counter').text(usCurrentFloor)
+            floorPath.removeClass("current-floor")
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
+        }
+    })
+    counterDown.on('click', function(){
+        if(currentFloor > 2){
+            currentFloor--
+            usCurrentFloor = currentFloor.toLocaleString('en-US',{minimumIntegerDigits: 2, useGrouping: false})
+            $('.counter').text(usCurrentFloor)
+            floorPath.removeClass("current-floor")
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
+        }
+    })
+});
